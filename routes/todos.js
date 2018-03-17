0a9be8f4c87ef7ededa85720673ac667f842331e
Joelle Fernandez <joellepfernandez@gmail.com>
2018-03-17 03:43:50 +0000

    added get, put, post, delete, and helpers

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../models");
+var helpers = require("../helpers/todos");
 
-router.get('/', function(req, res){
-res.send("HELLO FROM TODOS ROUTES");
-});
-
+router.route('/')
+ .get(helpers.getTodos)
+ .post(helpers.createTodo)
+ 
+router.route('/:todoId')
+  .get(helpers.getTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo)
+  
 module.exports = router;
