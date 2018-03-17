e155da46cf7f48995be8d9036e9a2eb1ad115a8c
Joelle Fernandez <joellepfernandez@gmail.com>
2018-03-17 01:33:30 +0000

    added port and express with todoRoutes

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+app = express();
+port = process.env.PORT || 3000
+
+var todoRoutes = require('./routes/todo')
+
+app.get('/', function(req, res){
+res.send("HELLO FROM THE ROOT ROUTE");
+});
+
+app.use('/api/todos', todoRoutes);
+
+app.listen(port, function(){
+console.log("APP IS RUNNING ON PORT" + process.env.PORT);
+})
